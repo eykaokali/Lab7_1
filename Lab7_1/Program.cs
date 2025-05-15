e86c863f8e3cee5511cc3b3987467b5c7561b301
Lisuko Лисуко <liza.shevchuk08@gmail.com>
2025-05-15 20:16:07 +0300

    Add project files.

---
+++ Lab7_1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lab7_1
+{
+
+    internal class Program
+    {
+        static List<TV> TVs;
+        static void printTVs()
+        {
+            foreach (TV tv in TVs)
+                Console.WriteLine(tv.info());
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            TVs = new List<TV>();
+            FileStream FS = new FileStream("TV.tv", FileMode.Open);
+            BinaryReader br = new BinaryReader(FS);
+            try
+            {
+                TV tv;
+                Console.WriteLine("Читаємо дані з файлу");
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    tv = new TV();
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        switch (i)
+                        {
+                            case 1:
+                                tv.Model = br.ReadString();
+                                break;
+                            case 2:
+                                tv.Brand = br.ReadString();
+                                break;
+                            case 3:
+                                tv.ScreenSize = br.ReadInt32();
+                                break;
+                            case 4:
+                                tv.Resolution = br.ReadString();
+                                break;
+                            case 5:
+                                tv.IsSmart = br.ReadBoolean();
+                                break;
+                            case 6:
+                                tv.Color = br.ReadString();
+                                break;
+                            case 7:
+                                tv.Price = br.ReadDouble();
+                                break;
+                        }
+                    }
+                    TVs.Add(tv);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: {0}", ex.Message);
+            }
+            finally
+            {
+                br.Close();
+            }
+            Console.WriteLine("Несортований перелік телевізорів {0}", TVs.Count);
+            printTVs();
+            TVs.Sort();
+            Console.WriteLine("Сортований перелік телевізорів {0}", TVs.Count);
+            printTVs();
+            Console.WriteLine("Додаємо новий запис LG");
+            TV tvLG = new TV("A55", "LG", 55, "1380x2250", false, "Чорний", 30000);
+            TVs.Add(tvLG);
+            TVs.Sort();
+            Console.WriteLine("Перелік телевізорів: {0}", TVs.Count);
+            printTVs();
+            Console.WriteLine("Видаляємо останій телевізор", TVs.Count);
+            TVs.RemoveAt(TVs.Count - 1);
+            Console.WriteLine("Перелік телевізорів: {0}", TVs.Count);
+            printTVs();
+
+        }
+    }
+}
---
+++ Lab7_1/TV.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab7_1
+{
+    public abstract class Household
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public abstract double Area
+        {
+            get;
+        }
+
+        void ChangePrice(float newPrice)
+        {
+            if (newPrice >= 0)
+                Price = newPrice;
+        }
+    }
+    public class TV : Household, IComparable<TV>
+    {
+        public int ScreenSize { get; set; }
+
+        public string Resolution { get; set; }
+        public bool IsSmart { get; set; }
+
+        public double x;
+        public override double Area
+        {
+            get
+            {
+                return Convert.ToDouble(16 * 9 *Math.Pow(Math.Sqrt(ScreenSize * ScreenSize / 337.0),2));
+            }
+        }
+
+        public TV()
+        {
+
+        }
+
+        public TV(string model, string brand, int screenSize, string resolution, bool isSmart, string color, double price)
+        {
+            Model = model;
+            Brand = brand;
+            ScreenSize = screenSize;
+            Resolution = resolution;
+            IsSmart = isSmart;
+            Color = color;
+            Price = price;
+        }
+
+        public int CompareTo(TV? other)
+        {
+            if (other == null) return 1;
+            return Area.CompareTo(other.Area);
+        }
+
+        public string info()
+        {
+            return $"Model: {Model}, Brand: {Brand}, ScreenSize: {ScreenSize}, Resolution: {Resolution}, IsSmart: {IsSmart}, Color: {Color}, Price: {Price}, Area {Area}";
+        }
+    }
+}
